f1510d572c3515b163bb00ac8e93fe562125688d
JplsMura <joaopedrolima.s@hotmail.com>
2021-11-09 13:07:51 -0300

    Adicionado funcao de criacao de novo usuario em repositorie, e validando o campo CPF antes do cadastro. Pois o mesmo aplica o mutator somente ao instancia um novo usuario do model User

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -17,4 +17,24 @@ class UserRepository
     {
         return $this->entity->get();
     }
+
+    public function createNewCourse(array $data)
+    {
+        $user = new User;
+        $user->name = $data['name'];
+        $user->registration = $data['registration'];
+        $user->email = $data['email'];
+        $user->cpf = $data['cpf'];
+
+        $cpf = User::select('*')
+                    ->where('cpf', $user->cpf)
+                    ->exists();
+
+        if($cpf === true){
+            return back()->withInput()->with('msgError', 'Este CPF já existe, por favor tente outro !');
+        }
+
+        $user->save();
+        return $user;
+    }
 }
