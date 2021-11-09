e8934e365fe5693412f0d0ad97778b3f0bbd3fc8
JplsMura <joaopedrolima.s@hotmail.com>
2021-11-09 13:06:34 -0300

    Adicionando funcao de criacao de novo usuario e validando campos ja existentes no banco e reportando a mensagem ao usuario

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Repositories\UserRepository;
+use App\User;
 
 class UserService
 {
@@ -16,5 +17,26 @@ class UserService
     public function getDados()
     {
         return $this->repository->getAllData();
+    }
+
+    public function createNewUser(array $data)
+    {
+        $registration = User::select('*')
+                            ->where('registration', $data['registration']
+                            )->exists();
+
+        if($registration === true){
+            return back()->withInput()->with('msgError', 'Este Número de Matricula já existe, por favor tente outro !');
+        }
+
+        $email = User::select('*')
+                        ->where('email', $data['email'])
+                        ->exists();
+
+        if($email === true){
+            return back()->withInput()->with('msgError', 'Este E-mail já existe, por favor tente outro !');
+        }
+
+        return $this->repository->createNewCourse($data);
     }
 }
